20ca4325627c2189e5df76fbb31fd285f10391dc
Santhosh <santhoshprogrammer94@gmail.com>
2021-02-07 14:48:46 +0530

    git issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ module.exports = {
         },
         {label: 'Blog', position: 'left',  href: 'https://programming-stacks.blogspot.com/',},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/FlyweightSoftwares/programming-stacks',
           label: 'GitHub',
           position: 'right',
         },
@@ -72,7 +72,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/FlyweightSoftwares/programming-stacks',
             },
           ],
         },
@@ -88,13 +88,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/FlyweightSoftwares/programming-stacks/edit/master/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/FlyweightSoftwares/programming-stacks/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
